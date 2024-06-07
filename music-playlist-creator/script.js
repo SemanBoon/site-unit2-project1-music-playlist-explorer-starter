21d2ab95dd0a3dcd36d5818e06b85f4b6da6513a
Sewa Benson <sewabenson@meta.com>
2024-06-07 14:02:17 -0700

    Update JS Landing Page

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,24 @@
+function openModal() {
+    document.getElementById('my-modal').style.display = "block";
+    document.body.classList.add('modal-open');
+  }
 
+  function closeModal() {
+    document.getElementById('my-modal').style.display = "none";
+    document.body.classList.remove('modal-open');
+  }
+
+
+  window.addEventListener('click', function(event) {
+      if (event.target == document.getElementById('my-modal')) {
+          document.getElementById('my-modal').style.display = "none";
+      }
+  });
+
+  function IncreaseLikes(event) {
+      event.stopPropagation();
+      var likesCountElement = event.target.nextElementSibling;
+      var likesCount = parseInt(likesCountElement.textContent);
+      likesCount++;
+      likesCountElement.textContent = likesCount;
+  }
